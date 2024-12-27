08629ad01692b187935829784420f575eb1589a2
Abir Hasan <abirsonet563@gmail.com>
2024-12-27 04:44:12 +0000

    Simple Calculator Completed

--- server.js
+++ server.js
@@ -13,9 +13,35 @@ app.get("/", (req, res) => {
 });
 
 app.post('/', (req, res) =>{
-  const number1= req.body.number1;
-  const number2 = req.body.number2;
+  const number1= parseFloat(req.body.number1);
+  const number2 = parseFloat(req.body.number2);
   const operation = req.body.operation;
+
+  let result;
+
+  switch(operation) {
+    case "add":
+        result = number1 + number2;
+        break;
+
+    case "subtract":
+        result = number1 - number2;
+        break;
+
+    case "multiply":
+        result = number1 * number2;
+        break;
+
+    case "divide":
+        result = number1 / number2;
+        break;
+
+        default:
+            result ="Invalid Operation";
+            break;    
+  }
+  
+  res.send("<h1 style='font-size: 58px; color:blue; position:absolute; top:40%; left: 40%'>Result:" + result + "</h1>");
 })
 
 app.listen(PORT, () => {
