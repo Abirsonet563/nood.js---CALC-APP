72812d861a0e8def33c81b432d330b34281d6f07
Abir Hasan <abirsonet563@gmail.com>
2024-12-27 04:27:09 +0000

    Grab the Submited Number and Operation

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const path = require('path');
 
 app.use(express.static("public"))
 
-const PORT = 8800
+app.use(express.urlencoded({extended:true}));
+
+const PORT = 8800;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
+});
+
+app.post('/', (req, res) =>{
+  const number1= req.body.number1;
+  const number2 = req.body.number2;
+  const operation = req.body.operation;
 })
 
 app.listen(PORT, () => {
