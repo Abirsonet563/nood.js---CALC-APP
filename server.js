bfcb12d57a6f4970627b5daf56878e054fd67955
Abir Hasan <abirsonet563@gmail.com>
2024-12-27 03:49:13 +0000

    Done HTML Coding Part

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get("/", (req, res) => {
-    res.send('I am ROOT ')
-})
+app.use(express.static("public"))
 
 const PORT = 8800
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
 app.listen(PORT, () => {
     console.log(`Server Started on Port ${PORT}`)
 })
